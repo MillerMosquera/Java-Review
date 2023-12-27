a9a00db0f5661c7406b932fe1e94f001b2432c1c
Miller Mosquera <millerpalacios16@gmail.com>
2023-12-26 21:45:02 -0500

    Generics

---
+++ src/Generics/GenericsExample.java
@@ -0,0 +1,1 @@
+package Generics;public class GenericsExample<T> {    private T value;    public GenericsExample(T value){        this.value = value;    }    public T getValue(){        return value;    }}
