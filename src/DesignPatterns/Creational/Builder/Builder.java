aac659060e30c7e98aa16c29b69e4630d8d9e9c6
Miller Mosquera <millerpalacios16@gmail.com>
2023-12-26 23:01:17 -0500

    Desing Patterns - Creational

---
+++ src/DesignPatterns/Creational/Builder/Builder.java
@@ -0,0 +1,1 @@
+package DesignPatterns.Creational.Builder;public interface Builder {    public void reset();    public void setSeats(int seats);    public void setEngine(String engine);    public void setTripComputer();    public void setGPS();}
---
+++ src/DesignPatterns/Creational/Builder/Manual.java
@@ -0,0 +1,1 @@
+package DesignPatterns.Creational.Builder;import lombok.Getter;import lombok.Setter;import lombok.ToString;@Getter@Setter@ToStringpublic class Manual {    public int seats;    public String engine;    public boolean tripComputer;    public boolean gps;    public Manual() {    }}
