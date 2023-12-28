33e08260d1e97a52e73e75a27b5bf540409aa22a
Miller Mosquera <44532720+MillerMosquera@users.noreply.github.com>
2023-12-27 19:21:21 -0500

    Update DecoratorDemo.java

--- src/DesignPatterns/Structural/Decorator/DecoratorDemo.java
+++ src/DesignPatterns/Structural/Decorator/DecoratorDemo.java
@@ -1,1 +1,15 @@
-package DesignPatterns.Structural.Decorator;public class DecoratorDemo {    public static void main(String[] args) {        ICoffee coffee = new BasicCoffee();        coffee = new SugarDecorator(coffee);        coffee = new MilkDecorator(coffee);        System.out.println(coffee.GetDescription());        System.out.println(coffee.GetCost());    }}
+package DesignPatterns.Structural.Decorator;
+
+public class DecoratorDemo {
+    public static void main(String[] args) {
+
+      // Permite añadir funcionalidades a objetos colocando estos objetos dentro de objetos encapsuladores especiales que contienen estas funcionalidades.
+        ICoffee coffee = new BasicCoffee();
+        coffee = new SugarDecorator(coffee);
+        coffee = new MilkDecorator(coffee);
+        System.out.println(coffee.GetDescription());
+        System.out.println(coffee.GetCost());
+
+
+    }
+}
