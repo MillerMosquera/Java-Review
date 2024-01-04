e446c1b68c27c996c7d600a3a271f796beda605c
Miller Mosquera <millerpalacios16@gmail.com>
2024-01-03 22:17:17 -0500

    update optional

---
+++ src/FunctionalProgramming/Optional/models/Computador.java
@@ -0,0 +1,1 @@
+package FunctionalProgramming.Optional.models;import lombok.*;@Getter@Setter@ToStringpublic class Computador {    private String nombre;    private String modelo;    public Computador(String nombre, String modelo) {        this.nombre = nombre;        this.modelo = modelo;    }}
---
+++ src/FunctionalProgramming/Optional/repositorio/IRepositorio.java
@@ -0,0 +1,1 @@
+package FunctionalProgramming.Optional.repositorio;import FunctionalProgramming.Optional.models.Computador;import java.util.Optional;public interface IRepositorio<T> {    Optional<Computador> filtrar(String nombre);}
