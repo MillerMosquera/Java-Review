3182377c539aeb5c0983d87591dd0206e32a874c
Miller Mosquera <millerpalacios16@gmail.com>
2025-03-30 16:25:20 -0500

    agregando nuevas estructuras de datos

---
+++ src/DataStructure/TreeSets.java
@@ -0,0 +1,1 @@
+package DataStructure;import java.util.TreeSet;public class TreeSets {    public static void main(String[] args) {        //Almacena elementos únicos de manera ordenada        TreeSet<Integer> set = new TreeSet<>();        set.add(1);        set.add(4);        set.add(5);        set.add(2);        System.out.println(set);    }}
